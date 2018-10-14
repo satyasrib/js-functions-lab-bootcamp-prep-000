69c9fbc71b3fd85442f97bd19779f676a2f2ddfa
Satyasri Varaganti <satyasrib@gmail.com>
2018-10-14 17:08:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 function happyHolidays() {
   return `${"Happy holidays!"}`
 }
-function happyHolidaysTo(happy holidays,name) {
+function happyHolidaysTo() {
   return `${Happy holidays}, ${name}!`
 }
